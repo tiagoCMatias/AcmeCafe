bd6579e8e91ddb55ab460724f2f84873b68893ca
Tiago Matias <tiago.matias92@gmail.com>
2018-03-02 12:09:04 +0000

    Base Project Structures

---
+++ Client-App/app/src/main/java/com/example/tiagomatias/acme_client/AcmeMain.java
@@ -0,0 +1,13 @@
+package com.example.tiagomatias.acme_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcmeMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acme_main);
+    }
+}
---
+++ NFC-App/app/src/main/java/com/example/tiagomatias/acme_nfc/NFCMain.java
@@ -0,0 +1,13 @@
+package com.example.tiagomatias.acme_nfc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NFCMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_main);
+    }
+}
