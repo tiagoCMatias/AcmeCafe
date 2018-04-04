2b2703552778266e0cf2e57c657fe2e8cb035363
Henrique Lopes Cardoso <hlc@zarcoapp.com>
2018-04-04 16:11:08 +0100

    Assinatura testes

--- Client-App/app/src/main/java/com/example/tiagomatias/acme_client/SendOrder.java
+++ Client-App/app/src/main/java/com/example/tiagomatias/acme_client/SendOrder.java
@@ -19,6 +19,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -27,6 +37,12 @@ public class SendOrder implements Runnable {
 
     Order order;
 
+    //ASSINATURA
+    PrivateKey privateKey;
+    Signature sg;
+    byte[] assinatura;
+
+
     public SendOrder(Order order){
         this.order = order;
     }
@@ -34,6 +50,9 @@ public class SendOrder implements Runnable {
     @Override
     public void run()
     {
+
+        makeBytesArray(this.order);
+
         System.out.println("RUNNABLLEMELMELMe");
         Map<String, String> data = new HashMap<>();
         JsonObject jOrderType = new JsonObject();
@@ -65,8 +84,17 @@ public class SendOrder implements Runnable {
             }
             jOrderType.addProperty("user",  order.getUserId() );
             jOrderType.addProperty("price",  order.getPrice() );
+
+
+
+            //ASSINATURA
+            jOrderType.addProperty("assinatura", this.assinatura.toString());
+
+
             jOrderType.add("products",  jProductData );
             jOrderType.add("vouchers",  jVoucherData );
+
+            System.out.println(jOrderType);
         }
         catch (Exception e) {
             System.out.println(e.toString());
@@ -143,6 +171,103 @@ public class SendOrder implements Runnable {
         System.out.println(response.toString());
 
         return response.toString();
+    }
+
+
+    // ASSINATURA (PARA APAGAR)
+
+    public void makeBytesArray(Order order){
+
+        // UUID tem 16 bytes
+        //INT tem 4bytes
+        // assinatura tem 48 bytes
+        // usar 1 byte para o numero de produtos
+
+        byte[] userIdByte = order.getUserId().getBytes();
+        byte sizeOfUserIdByte = (byte) userIdByte.length;
+
+        int numberOfProducts = order.getProducts().size();
+        byte numberOfProductsByte = (byte) numberOfProducts;
+        int numberOfVouchers = order.getVouchers().size();
+        byte numberOfVouchersByte = (byte) numberOfVouchers;
+
+
+
+        //ESTRUTURA: tamanho do ID (1 byte), ID, tamanho dos produtos(1 byte), produtos(tamanho * 25 pq é 1 byte para a quantidade e 24byte para o nome), tamanho dos vouchers(1 byte), vouchers (tamanho * 24)
+        Integer sizeOfByteBuffer = 1 + sizeOfUserIdByte + 1 + numberOfProducts * 2 + 1 + numberOfVouchers * 12 + 64;
+        int sizeUsed = 0;
+
+        ByteBuffer bb = ByteBuffer.allocate(sizeOfByteBuffer);
+
+        bb.put(sizeOfUserIdByte);
+        sizeUsed += 1;
+        bb.put(userIdByte);
+        sizeUsed += sizeOfUserIdByte;
+        bb.put((byte) (numberOfProductsByte * 2));
+        sizeUsed += 1;
+        //FOREACH products
+        for (OrderProduct p: order.getProducts()) {
+            bb.put((byte) p.getQuantity());
+            bb.put((byte) p.getTag_number());
+            sizeUsed+= 2;
+        }
+
+        bb.put((byte) (numberOfVouchersByte));
+        sizeUsed += 1;
+        //FOREACH vouchers
+        for (Voucher v: order.getVouchers()) {
+            bb.put(v.getId().getBytes());
+            sizeUsed+= 12;
+        }
+
+        //Assinatura
+        try {
+            KeyStore keystore = KeyStore.getInstance("AndroidKeyStore");
+            keystore.load(null);
+
+            KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry)keystore.getEntry("keys", null);
+            privateKey = privateKeyEntry.getPrivateKey();
+
+            sg = Signature.getInstance("SHA1WithRSA");                    // for signing with the stated algorithm (Signing and verifying object)
+            sg.initSign(privateKey);                                             // supply the private key
+            sg.update(bb.array());                                             // define the data to sign
+            assinatura = sg.sign();
+            sizeUsed+= assinatura.length;
+            System.out.println("Sign SiZE: " + assinatura.length);
+            System.out.println("USED: " + sizeUsed);
+            System.out.println(bb.array().length);
+            System.out.println("ASS: " + assinatura.toString());
+            bb.put(assinatura);//BufferOverflowException
+
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableEntryException e) {
+            e.printStackTrace();
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("ID BYTE :" + userIdByte);
+        System.out.println("SIZE ID:" + sizeOfUserIdByte);
+        System.out.println("SIZE P BYte:" + numberOfProductsByte);
+        System.out.println("SIZE V Byte:" + numberOfVouchersByte);
+
+        System.out.println("SIZE BB: " + bb.array().length);
+        System.out.println("BB STRING:");
+
+        for (byte b: bb.array()) {
+            System.out.println(b);
+        }
+
     }
 }
 
